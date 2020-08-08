19c9865795771e17d1bb80acf7c7061e3d9d8db9
Akshay Kumar M S <akshay.k@thinkpalm.com>
2020-08-09 01:03:16 +0530

    Updates

---
+++ precache-manifest.84de7abaca3d50b7f4c1b9b295548cde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61e51901f3955e6f6b0a464921eadbb",
+    "url": "/github.io/monsters/index.html"
+  },
+  {
+    "revision": "bdef6e5d710615836106",
+    "url": "/github.io/monsters/static/css/main.023d5651.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/github.io/monsters/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/monsters/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdef6e5d710615836106",
+    "url": "/github.io/monsters/static/js/main.da35be1c.chunk.js"
+  },
+  {
+    "revision": "014d153cc333e92dbdce",
+    "url": "/github.io/monsters/static/js/runtime-main.7ee31f7c.js"
+  }
+]);
